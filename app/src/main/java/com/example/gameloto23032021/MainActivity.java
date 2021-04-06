789f00e215b1f32674f59fd3c4a82b18a223bffb
Tuan <minhtuanle97@gmail.com>
2021-04-06 13:57:07 +0700

    cach xu li random

--- app/src/main/java/com/example/gameloto23032021/MainActivity.java
+++ app/src/main/java/com/example/gameloto23032021/MainActivity.java
@@ -13,16 +13,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Math
         //1: Làm tròn
-        float st1 = 0.5f;
-        // round : dưới thì xuống 0, trên thì lên 1
-        float valueSt1 = Math.round(st1);
-        // làm tròn thành 1 hết
-        double valueSt2 = Math.ceil(Double.parseDouble(st1 + ""));
-        // làm tròn về 0
-        double valueSt3 = Math.floor(Double.parseDouble(st1 + ""));
-
-        Log.d("AAA",valueSt1 + "" );
-        Log.d("BBB",valueSt2 + "" );
-        Log.d("CCC",valueSt3 + "" );
+//        float st1 = 0.5f;
+//        // round : dưới thì xuống 0, trên thì lên 1
+//        float valueSt1 = Math.round(st1);
+//        // làm tròn thành 1 hết
+//        double valueSt2 = Math.ceil(Double.parseDouble(st1 + ""));
+//        // làm tròn về 0
+//        double valueSt3 = Math.floor(Double.parseDouble(st1 + ""));
+//
+//        Log.d("AAA",valueSt1 + "" );
+//        Log.d("BBB",valueSt2 + "" );
+//        Log.d("CCC",valueSt3 + "" );
+        double value = Math.floor(Math.random()*6);
+        Log.d("AAA", value + "");
     }
 }
